301d23d247dfe73768bc53eda6aae166713fbd47
riousenkai <johnelijah.fajardo@gmail.com>
2021-10-07 01:19:14 -0500

    finished frontend and backend

--- frontend/src/components/Navigation/Navigation.js
+++ frontend/src/components/Navigation/Navigation.js
@@ -2,10 +2,25 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
+import LoginFormModal from "../LoginFormModal";
 import "./Navigation.css";
 
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector((state) => state.session.user);
+
+  const hide = () => {
+    const dropdown = document.querySelectorAll(".gundam-dropdown");
+    dropdown.forEach((e) => {
+      e.classList.add("hidden");
+    });
+  };
+
+  const show = () => {
+    const dropdown = document.querySelectorAll(".gundam-dropdown");
+    dropdown.forEach((e) => {
+      e.classList.remove("hidden");
+    });
+  };
 
   let sessionLinks;
   if (sessionUser) {
@@ -13,21 +28,52 @@ function Navigation({ isLoaded }) {
   } else {
     sessionLinks = (
       <>
-        <NavLink to="/login">Log In</NavLink>
-        <NavLink to="/signup">Sign Up</NavLink>
+        <NavLink to="/signup" className="gundam-dropdown">
+          Sign Up
+        </NavLink>
+        <LoginFormModal />
       </>
     );
   }
 
   return (
-    <ul>
-      <li>
-        <NavLink exact to="/">
-          Home
-        </NavLink>
-        {isLoaded && sessionLinks}
-      </li>
-    </ul>
+    <div className="header">
+      <NavLink to="/" className="header-icon">
+        <img id="img-top" src="header.png" />
+      </NavLink>
+      <input
+        className="search"
+        type="search"
+        placeholder="Search..."
+        onFocus={hide}
+        onBlur={show}
+      />
+      <div className="gundam-dropdown">
+        <button className="gundam-dropbtn">Gundam Kits</button>
+        <div className="gundam-dropdown-content">
+          <NavLink to="/">Link 1</NavLink>
+          <NavLink to="/">Link 2</NavLink>
+          <NavLink to="/">Link 3</NavLink>
+        </div>
+      </div>
+      <div className="gundam-dropdown">
+        <button className="gundam-dropbtn">Community</button>
+        <div className="gundam-dropdown-content">
+          <NavLink to="/">Link 1</NavLink>
+          <NavLink to="/">Link 2</NavLink>
+          <NavLink to="/">Link 3</NavLink>
+        </div>
+      </div>
+      <div className="gundam-dropdown">
+        <button className="gundam-dropbtn">Tools</button>
+        <div className="gundam-dropdown-content">
+          <NavLink to="/">Link 1</NavLink>
+          <NavLink to="/">Link 2</NavLink>
+          <NavLink to="/">Link 3</NavLink>
+        </div>
+      </div>
+      {isLoaded && sessionLinks}
+    </div>
   );
 }
 
--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -30,7 +30,7 @@ function ProfileButton({ user }) {
 
   return (
     <>
-      <button onClick={openMenu}>
+      <button onClick={openMenu} className="gundam-dropdown">
         <i className="fas fa-user-circle" />
       </button>
       {showMenu && (
