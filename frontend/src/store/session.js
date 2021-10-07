e4d81e0d0cf0d916c19cbfcfafaadd83107d683f
riousenkai <johnelijah.fajardo@gmail.com>
2021-10-06 23:39:20 -0500

    added signup form

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -24,6 +24,28 @@ export const logout = () => async (dispatch) => {
     return res;
 }
 
+export const restoreUser = () => async dispatch => {
+    const response = await csrfFetch('/api/session');
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+  };
+
+export const signup = (user) => async (dispatch) => {
+    const { username, email, password } = user;
+    const response = await csrfFetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+    });
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+  };
+
 export const login = (user) => async (dispatch) => {
   const { credential, password } = user;
   const response = await csrfFetch('/api/session', {
