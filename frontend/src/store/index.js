90ea14bfe50e4cfd663437c17dead25e6f624f4c
riousenkai <johnelijah.fajardo@gmail.com>
2021-10-10 20:23:31 -0500

    added reducer to store index

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,11 +1,13 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import sessionReducer from './session';
+import gundamReducer from "./gundam";
 import userReducer from "./user";
 
 const rootReducer = combineReducers({
   session: sessionReducer,
-  user: userReducer
+  user: userReducer,
+  gundam: gundamReducer
 });
 
 let enhancer;
