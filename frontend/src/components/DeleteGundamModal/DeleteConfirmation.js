d8af9214520ca1f862a8516c45de11f81e75b557
riousenkai <johnelijah.fajardo@gmail.com>
2021-10-12 21:50:05 -0500

    styling on gundam page

--- frontend/src/components/DeleteGundamModal/DeleteConfirmation.js
+++ frontend/src/components/DeleteGundamModal/DeleteConfirmation.js
@@ -3,6 +3,7 @@ import { useHistory } from "react-router";
 import { deleteGundam } from "../../store/gundam";
 import { useShowModal } from "../../context/ShowModal";
 import { useSelector } from "react-redux";
+import "./DeleteConfirmation.css"
 
 const DeleteConfirmation = ({gundam}) => {
     const dispatch = useDispatch()
@@ -21,8 +22,10 @@ const DeleteConfirmation = ({gundam}) => {
     return (
         <div className="confirmation">
             <p className="confirmation-header">Are you sure you want to delete this post?</p>
+            <div className="confirmation-buttons">
             <button className="confirmation-yes" onClick={(e) => deleteGundams(gundam.id, e)}>Yes</button>
             <button className="confirmation-no" onClick={() => setNum(0)}>No</button>
+            </div>
         </div>
     )
 }
