9be77bd7b6cce36f06d4c6e904789f1fe2ec74a4
riousenkai <johnelijah.fajardo@gmail.com>
2021-10-13 22:38:44 -0500

    finished upvotes

--- backend/db/seeders/20211014033028-upvotes.js
+++ backend/db/seeders/20211014033028-upvotes.js
@@ -8,22 +8,32 @@ module.exports = {
         {
           user_id: 1,
           gundam_id: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           user_id: 2,
           gundam_id: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           user_id: 1,
           gundam_id: 3,
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           user_id: 1,
           gundam_id: 4,
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
         {
           user_id: 1,
           gundam_id: 5,
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
       ],
       {}
