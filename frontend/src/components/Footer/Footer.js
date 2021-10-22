b165786883afe5d94c8651e4c9d5193d756b8cdf
riousenkai <johnelijah.fajardo@gmail.com>
2021-10-22 17:20:38 -0500

    fixed footer

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import "./Footer.css";
 const footer = () => {
   return (
     <div className="footer">
-      <p className="footer-text">A project by Revan Fajardo</p>
+      <p className="footer-text">A project by John Elijah "Revan" Fajardo</p>
       <a href="https://www.linkedin.com/in/john-elijah-revan-fajardo-33a189a3/" target="_blank" rel="noreferrer">
         <i className="fab fa-linkedin"></i>
       </a>
